5dee9ca4c14c72dfc019137bb0654e7e39322bbf
danielprasetyo7952 <vyverdrake007@gmail.com>
2023-10-23 12:27:19 +0700

    Update - Layout - Requirements

--- func.py
+++ func.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import streamlit as st
+import tensorflow as tf
 
 def config():
     st.set_page_config(page_title="Forex Forecasting",
                        layout="wide",
-                       page_icon=":moneybag:")
+                       page_icon=":currency_exchange:")
     
 def load_data(sheet_name):
     url = f'https://docs.google.com/spreadsheets/d/1JDNv_mArl-GPIpxuWS5GxgVEwvjXocS1MrXGc6TYs8M/gviz/tq?tqx=out:csv&sheet={sheet_name}/IDR'
     df = pd.read_csv(url)
+    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y %H:%M:%S')
     return df
+
+# def load_model():
+#     model = tf.load_model()
+#     return model
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import streamlit as st
+import altair as alt
 import func as f
 
 f.config()
 
-st.title("Prediksi Harga Beli Mata Uang Asing Terhadap Rupiah")  
+st.title("Prediksi Harga Beli Mata Uang Asing Terhadap Rupiah")
+col1, col2 = st.columns([1, 2])
 
-col1, col2 = st.columns(2)
+def show_data(source):
+    chart = (
+        alt.Chart(source)
+        .mark_line()
+        .encode(
+            alt.X("Date"),
+            alt.Y("Close",
+                  scale=alt.Scale(domain=[min(source['Close']), max(source['Close'])])
+            )
+        )
+        .interactive()
+    )
+    
+    col2.subheader(forex_type)
+    col2.altair_chart(chart, use_container_width=True)
+    col2.write(f"Prediksi harga beli {forex_type} terhadap Rupiah untuk {duration} ke depan")
+
 with col1:
     forex_type = st.selectbox("Pilih Mata Uang Asing", ["USD", "EUR", "SGD"])
-    duration = st.selectbox("Pilih Durasi", ["1 Hari", "3 Hari", "5 Hari", "1 Minggu", "2 Minggu", "1 Bulan"])
-
+    duration = st.selectbox("Pilih Durasi", ["1 Hari", "3 Hari", "5 Hari", "10 Hari", "20 Hari"])
+    action = st.button("Prediksi")
+    
 with col2:
     data = f.load_data(forex_type)
-    st.line_chart(data['Close'])
+    if (action):
+        show_data(data)
+    
